bd205ddeea3c874b03df03059ac84ef7c29c798b
besufikad <07yilmabese@gmail.com>
2023-10-23 19:08:18 +0300

    t 0

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""reading api from third party"""
+from sys import argv
+import requests
+
+"""
+The code block starting with `if __name__ == '__main__':`
+is the main entry point of the script. It is executed when
+the script is run directly (not imported as a module).
+"""
+if __name__ == '__main__':
+    url = F"https://jsonplaceholder.typicode.com/users/{argv[1]}"
+    to_do = "/todos"
+    name = ""
+    with requests.get(url) as response:
+        name = response.json()['name']
+    url = url + to_do
+    task_completed = 0
+    with requests.get(url) as response:
+        response = response.json()
+        for i in response:
+            if i['completed'] is True:
+                task_completed += 1
+        print("Employee {} is done with tasks({}/{}):"
+              .format(name, task_completed, len(response)))
+        for i in response:
+            if i['completed'] is True:
+                print(F"\t {i['title']}")
