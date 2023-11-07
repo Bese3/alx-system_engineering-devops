e485011b56a991317b1c1e7da92ac7e2f900efd9
besufikad <07yilmabese@gmail.com>
2023-11-07 18:03:01 +0300

    t 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+import json
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    The function `number_of_subscribers`
+    returns the number of subscribers for a given subreddit by
+    making a request to the Reddit API.
+    """
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+    headers = {
+        'User-Agent': 'Python_Advanced_API_tasks'
+    }
+    url = F"https://www.reddit.com/r/{subreddit}/about.json"
+    with requests.get(url, headers=headers) as response:
+        return response.json()["data"]["subscribers"]
