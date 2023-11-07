7d7de0caa80df084ba5ca876c28348c334922473
besufikad <07yilmabese@gmail.com>
2023-11-07 19:13:35 +0300

    t 1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+using reddit API
+"""
+import json
+import requests
+
+
+def top_ten(subreddit):
+    """
+    The `top_ten` function retrieves the top ten posts from
+    a specified subreddit on Reddit and prints
+    their titles.
+    """
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+    headers = {
+        'User-Agent': 'Python_Advanced_API_tasks'
+    }
+    url = F"https://www.reddit.com/r/{subreddit}/hot.json"
+    with requests.get(url, headers=headers, params={'limit': 10}) as response:
+        if response.status_code >= 400:
+            return 0
+        posts = response.json()["data"]["children"]
+        if posts is None or posts[0]['kind'] != 't3':
+            print(None)
+        for i in posts:
+            print(i['data']['title'])
