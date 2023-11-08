884a6eba8a9ea6f187246cef3d03ff21aaf9336f
besufikad <07yilmabese@gmail.com>
2023-11-08 16:17:46 +0300

    strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # fixes Apache 500 error
-exec {'file_edit'
+exec {'file_edit':
     command => "sed -i 's/phpp/php/' /var/www/html/wp-settings.php",
     path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
